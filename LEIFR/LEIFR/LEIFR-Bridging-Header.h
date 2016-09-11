dde04f960e3013eef8e734ebf82c968a7f8cff1e
Jinghan <phantom.there@gmail.com>
2016-09-12 04:51:34 +0800

    link lib

---
+++ LEIFR/LEIFR/LEIFR-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <sqlite3.h>
+#import <spatialite/gaiageo.h>
+#import <spatialite.h>
