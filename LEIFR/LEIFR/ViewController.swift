13ee6e2757eeda6b671e1d962b0eb176a4a5ab6f
Jinghan <phantom.there@gmail.com>
2016-09-12 04:21:03 +0800

    Project setup

---
+++ LEIFR/LEIFR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LEIFR
+//
+//  Created by Jinghan Wang on 12/9/16.
+//  Copyright © 2016 Echx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
