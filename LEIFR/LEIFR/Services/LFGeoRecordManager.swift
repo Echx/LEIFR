af11c5b410096369156f716c96e7f3acdb2aba75
Lei Mingyu <uygnim.iel@gmail.com>
2016-09-12 19:12:38 +0800

    handle recording and flushing in record manager

---
+++ LEIFR/LEIFR/Services/LFGeoRecordManager.swift
@@ -0,0 +1,45 @@
+//
+//  LFGeoRecordManager.swift
+//  LEIFR
+//
+//  Created by Lei Mingyu on 12/9/16.
+//  Copyright © 2016 Echx. All rights reserved.
+//
+
+import UIKit
+
+class LFGeoRecordManager: NSObject {
+    private static let manager = LFGeoRecordManager()
+    private var bufferPath = LFPath()
+    private var mutex = pthread_mutex_t()
+    
+    class func sharedManager() -> LFGeoRecordManager {
+        return self.manager
+    }
+    
+    func recordPoint(newPoint: CLLocation) {
+        pthread_mutex_lock(&mutex)
+        defer {
+            pthread_mutex_unlock(&mutex)
+        }
+        
+        let coordinate = newPoint.coordinate
+        self.bufferPath.addPoint(latitude: coordinate.latitude, longitude: coordinate.longitude, altitude: newPoint.altitude)
+    }
+    
+    func flushPoint() {
+        pthread_mutex_lock(&mutex)
+        defer {
+            pthread_mutex_unlock(&mutex)
+        }
+        
+        let databaseManager = LFDatabaseManager.sharedManager()
+        databaseManager.savePath(bufferPath, completion: {
+            success in
+            
+            if !success {
+                self.flushPoint()
+            }
+        })
+    }
+}
