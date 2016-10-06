63a3e4fca05fab4b0ff7ffdbb51b3c61f67a5d07
Lei Mingyu <uygnim.iel@gmail.com>
2016-10-07 05:51:43 +0800

    add json wrapper and test

---
+++ LEIFR/LEIFR/Services/LFGeoJSONWrapper.swift
@@ -0,0 +1,24 @@
+//
+//  LFGeoJSONWrapper.swift
+//  LEIFR
+//
+//  Wrapping a general geoJSON data into a MapBox-reginizable format
+//
+//  Created by Lei Mingyu on 7/10/16.
+//  Copyright © 2016 Echx. All rights reserved.
+//
+
+import UIKit
+
+class LFGeoJSONWrapper: NSObject {
+    class func wrap(geometry: String) -> String? {
+        let wrappedResult = "{\"type\": \"FeatureCollection\", \"features\": [{\"type\": \"Feature\", \"geometry\":" + geometry + "}]}";
+        let data = wrappedResult.data(using: .utf8)!
+        if (try? JSONSerialization.jsonObject(with: data)) != nil {
+            return wrappedResult
+        } else {
+            print("invalid JSON!")
+            return nil
+        }
+    }
+}
