2fc3bfdecd9db147ebee938c8bba03b28b08d18d
Jinghan Wang <jinghan.w@icloud.com>
2016-10-26 05:23:19 +0800

    grid

--- LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
+++ LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class LFGeoPointsOverlayRenderer: MKOverlayRenderer {
 	
+	var gridSize: CGFloat = 10000
+	var filledPercentage: CGFloat = 0.9
+	
 	override func canDraw(_ mapRect: MKMapRect, zoomScale: MKZoomScale) -> Bool {
 		return true
 	}
 	
 	override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+		
+		gridSize = 1 / zoomScale * 25
+		
 		let cgRect = rect(for: mapRect)
-		context.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.2)
-		context.fill(cgRect)
+		context.setFillColor(red: 1, green: 1, blue: 1, alpha: 0.2)
+		
+		let xNumberOfGrid = Int(ceil(cgRect.width / gridSize)) * 2
+		let yNumberOfGrid = Int(ceil(cgRect.width) / gridSize) * 2
+		let minX = CGFloat(Int(cgRect.minX) / Int(gridSize) * Int(gridSize))
+		let minY = CGFloat(Int(cgRect.minY) / Int(gridSize) * Int(gridSize))
+		
+		for i in 0..<xNumberOfGrid {
+			for j in 0..<yNumberOfGrid {
+				let x = CGFloat(i) * gridSize + minX
+				let y = CGFloat(j) * gridSize + minY
+				let size = gridSize * filledPercentage
+				let rect = CGRect(x: x, y: y, width: size, height: size)
+				context.fill(rect)
+			}
+		}
 	}
 	
 }
