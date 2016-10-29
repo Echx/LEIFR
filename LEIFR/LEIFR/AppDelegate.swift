1ad5fad8b8b264508ea59fb16396afe3d4eb879f
Lei Mingyu <uygnim.iel@gmail.com>
2016-10-30 02:13:03 +0800

    call it database instead of Realm might be better

--- LEIFR/LEIFR/AppDelegate.swift
+++ LEIFR/LEIFR/AppDelegate.swift
@@ -24,8 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configureLocationManager()
 		FontBlaster.blast()
 		
-//        LFCachedDatabaseManager.shared.destroyRealm()
-		
 		return true
 	}
 
--- LEIFR/LEIFR/Services/LFCachedDatabaseManager.swift
+++ LEIFR/LEIFR/Services/LFCachedDatabaseManager.swift
@@ -72,14 +72,14 @@ class LFCachedDatabaseManager: NSObject {
         return currentLevels[0].points.map{MKMapPoint(x: Double($0.x), y: Double($0.y))}
     }
     
-    func clearRealm() {
+    func clearDatabase() {
         let realm = try! Realm()
         try! realm.write {
             realm.deleteAll()
         }
     }
     
-    func destroyRealm() {
+    func destroyDatabase() {
         let databaseDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
         let array = try! FileManager.default.contentsOfDirectory(atPath: databaseDirectory)
         
