b6792d3acfa20b6ec42a845ac57a28f5f6b67683
Jinghan Wang <jinghan.w@icloud.com>
2016-11-02 00:55:27 +0800

    loading view controller

---
+++ LEIFR/LEIFR/ViewControllers/LFLoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LFLoadingViewController.swift
+//  LEIFR
+//
+//  Created by Jinghan Wang on 2/11/16.
+//  Copyright © 2016 Echx. All rights reserved.
+//
+
+import UIKit
+
+class LFLoadingViewController: LFViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
