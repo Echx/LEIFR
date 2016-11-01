11c9498ea7b1186f47ea95b2acde74563af01f3c
Jinghan Wang <jinghan.w@icloud.com>
2016-11-02 03:37:18 +0800

    temp fix for tile missing

--- LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
+++ LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
@@ -24,19 +24,23 @@ class LFGeoPointsOverlayRenderer: MKOverlayRenderer {
             guard cachedPoints.count > 0 else {
                 return
             }
+			
 			let gridSize = self.gridSizeDrawn(for: zoomScale)
             let counts = cachedPoints.map{$0.count}
             let maxCount = counts.max()!
             let minCount = counts.min()!
             let maxAlpha = 0.9
             let minAlpha = 0.2
+			
+			
 			for cachedPoint in cachedPoints {
                 let mapPoint = MKMapPoint(x: Double(cachedPoint.x), y: Double(cachedPoint.y))
 				let point = self.point(for: mapPoint)
 				let rect = CGRect(x: point.x, y: point.y, width: gridSize, height: gridSize)
                 let count = cachedPoint.count
-                let weightedAlpha = Double(count - minCount) / Double(maxCount - count) * (maxAlpha - minAlpha) + minAlpha
-                context.setFillColor(red: 0, green: 0, blue: 0, alpha: CGFloat(weightedAlpha))
+                let weightedAlpha = Double(count - minCount) / Double(maxCount - minCount) * (maxAlpha - minAlpha) + minAlpha
+				//TODO: fix weightedAlpha for the cases where maxCount == minCount
+                context.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.2)
 				context.fill(rect)
 			}
 		}
