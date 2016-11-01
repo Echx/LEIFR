fc92ae5b3208724f49b1535e9ab306091bc0e394
Jinghan Wang <jinghan.w@icloud.com>
2016-11-01 23:39:13 +0800

    Revert "renderer update"
    This reverts commit e964a9dba83e935cc071dfdf3eebff555985cbd1.

--- LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
+++ LEIFR/LEIFR/Renderer/LFGeoPointsOverlayRenderer.swift
@@ -12,122 +12,44 @@ class LFGeoPointsOverlayRenderer: MKOverlayRenderer {
 	
 	var filledPercentage: CGFloat = 0.9
 	
-	fileprivate var cache = [String: [MKMapPoint]]()
+	fileprivate var cache = [String: [CGPoint]]()
 	fileprivate var cacheAccessQueue = DispatchQueue(label: "CACHE_QUEUE")
 	
 	
 	override func canDraw(_ mapRect: MKMapRect, zoomScale: MKZoomScale) -> Bool {
-		return true
-		
-//		let key = self.cacheKey(for: mapRect, zoomScale: zoomScale)
-//		if let _ = cache[key] {
-//			print("true \(key)")
-//			return true
-//		} else {
-//			print("false \(key)")
-//			let region = MKCoordinateRegionForMapRect(mapRect)
-//			let gridSize = self.gridSize(for: zoomScale)
-//			LFDatabaseManager.sharedManager().getPointsInRegion(region, gridSize: gridSize, completion: {
-//				coordinates in
-//				let points = coordinates.map({ return self.point(for: MKMapPointForCoordinate($0)) })
-//				self.cacheAccessQueue.async {
-//					self.cache[key] = points
-//					DispatchQueue.main.async {
-//						self.setNeedsDisplayIn(mapRect, zoomScale: zoomScale)
-//					}
-//				}
-//			})
-//			
-//			return true
-//		}
+		let key = self.cacheKey(for: mapRect, zoomScale: zoomScale)
+		if let _ = cache[key] {
+			return true
+		} else {
+			let region = MKCoordinateRegionForMapRect(mapRect)
+			let gridSize = self.gridSize(for: zoomScale)
+			LFDatabaseManager.sharedManager().getPointsInRegion(region, gridSize: gridSize, completion: {
+				coordinates in
+				let points = coordinates.map({ return self.point(for: MKMapPointForCoordinate($0)) })
+				self.cacheAccessQueue.async {
+					self.cache[key] = points
+					self.setNeedsDisplayIn(mapRect, zoomScale: zoomScale)
+				}
+			})
+			
+			return false
+		}
 	}
 	
 	override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
 		let key = self.cacheKey(for: mapRect, zoomScale: zoomScale)
-		var cachedPoints: [MKMapPoint]?
-		print("draw \(key)")
+		var cachedPoints: [CGPoint]?
+		
 		cacheAccessQueue.sync {
 			cachedPoints = self.cache[key]
 		}
 		
 		if let points = cachedPoints {
-			let gridSizeDrawn = self.gridSizeDrawn(for: zoomScale)
+			let gridSize = self.gridSizeDrawn(for: zoomScale)
 			context.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.1)
-			for mapPoint in points {
-				let point = self.point(for: mapPoint)
-				let rect = CGRect(x: point.x - gridSizeDrawn/2, y: point.y - gridSizeDrawn/2, width: gridSizeDrawn, height: gridSizeDrawn)
-				
+			for point in points {
+				let rect = CGRect(x: point.x - gridSize/2, y: point.y - gridSize/2, width: gridSize, height: gridSize)
 				context.fill(rect)
-			}
-		} else {
-			let origin = mapRect.origin
-			let width = Int(mapRect.size.width * 2)
-			let newX = Int(origin.x) / width * width
-			let newY = Int(origin.y) / width * width
-			let newMapRect = MKMapRect(origin: MKMapPoint(x: Double(newX), y:Double(newY)), size: MKMapSize(width: Double(width), height: Double(width)))
-			let newKey = self.cacheKey(for: newMapRect, zoomScale: zoomScale / 2)
-			cacheAccessQueue.sync {
-				cachedPoints = self.cache[newKey]
-			}
-			
-			var mapPoints = [MKMapPoint]()
-
-			if let points = cachedPoints {
-				let gridSizeDrawn = self.gridSizeDrawn(for: zoomScale)
-				context.setFillColor(red: 0, green: 0, blue: 0, alpha: 0.1)
-				for mapPoint in points {
-					let halfSize = Double(gridSizeDrawn * 1.242)
-					do {
-						mapPoints.append(mapPoint)
-						let point = self.point(for: mapPoint)
-						let rect = CGRect(x: point.x - gridSizeDrawn/2, y: point.y - gridSizeDrawn/2, width: gridSizeDrawn, height: gridSizeDrawn)
-						context.fill(rect)
-					}
-					
-					do {
-						let fakeMapPoint = MKMapPointMake(mapPoint.x + halfSize, mapPoint.y)
-						mapPoints.append(fakeMapPoint)
-						let point = self.point(for: fakeMapPoint)
-						let rect = CGRect(x: point.x - gridSizeDrawn/2, y: point.y - gridSizeDrawn/2, width: gridSizeDrawn, height: gridSizeDrawn)
-						context.fill(rect)
-					}
-					
-					do {
-						let fakeMapPoint = MKMapPointMake(mapPoint.x, mapPoint.y + halfSize)
-						mapPoints.append(fakeMapPoint)
-						let point = self.point(for: fakeMapPoint)
-						let rect = CGRect(x: point.x - gridSizeDrawn/2, y: point.y - gridSizeDrawn/2, width: gridSizeDrawn, height: gridSizeDrawn)
-						context.fill(rect)
-					}
-					
-					do {
-						let fakeMapPoint = MKMapPointMake(mapPoint.x + halfSize, mapPoint.y + halfSize)
-						mapPoints.append(fakeMapPoint)
-						let point = self.point(for: fakeMapPoint)
-						let rect = CGRect(x: point.x - gridSizeDrawn/2, y: point.y - gridSizeDrawn/2, width: gridSizeDrawn, height: gridSizeDrawn)
-						context.fill(rect)
-					}
-				}
-				
-				self.cacheAccessQueue.sync {
-					self.cache[key] = mapPoints
-				}
-			}
-			
-			DispatchQueue(label: key).async {
-				let region = MKCoordinateRegionForMapRect(mapRect)
-				let gridSize = self.gridSize(for: zoomScale)
-				LFDatabaseManager.sharedManager().getPointsInRegion(region, gridSize: gridSize, completion: {
-					coordinates in
-					let mapPoints = coordinates.map({ return MKMapPointForCoordinate($0) })
-					self.cacheAccessQueue.sync {
-						self.cache[key] = mapPoints
-					}
-					
-					DispatchQueue.main.async {
-						self.setNeedsDisplayIn(mapRect, zoomScale: zoomScale)
-					}
-				})
 			}
 		}
 	}
@@ -137,11 +59,11 @@ class LFGeoPointsOverlayRenderer: MKOverlayRenderer {
 	}
 	
 	fileprivate func gridSize(for zoomScale: MKZoomScale) -> Double {
-		return 1 / Double(zoomScale) / 50000
+		return 1 / Double(zoomScale) / 20000
 	}
 	
 	fileprivate func gridSizeDrawn(for zoomScale: MKZoomScale) -> CGFloat {
-		return CGFloat(self.gridSize(for: zoomScale)) * 600000
+		return 1 / zoomScale * 30
 	}
 	
 }
