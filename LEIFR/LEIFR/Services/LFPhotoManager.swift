e2f3437f1e64ce33e61155bafa00a00d547e2128
Jinghan Wang <jinghan.w@icloud.com>
2017-01-29 17:29:43 +0800

    caching for assets

--- LEIFR/LEIFR/Services/LFPhotoManager.swift
+++ LEIFR/LEIFR/Services/LFPhotoManager.swift
@@ -12,21 +12,37 @@ import Photos
 class LFPhotoManager: NSObject {
 	static let shared = LFPhotoManager()
 	let thumbnailSize = CGSize(width: 200, height: 200)
+	let imageManager = PHCachingImageManager()
 	
-	func fetchAssets(from fromDate: Date, till endDate: Date) -> PHFetchResult<PHAsset> {
+	func fetchAssets(from fromDate: Date, till endDate: Date) -> [PHAsset] {
 		let fetchOptions = PHFetchOptions()
 		fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
 		fetchOptions.predicate = NSPredicate(format: "(creationDate >= %@) AND (creationDate <= %@) AND (mediaType == %ld)", fromDate as NSDate, endDate as NSDate, PHAssetMediaType.image.rawValue)
 		let fetchResult = PHAsset.fetchAssets(with: fetchOptions)
-		return fetchResult
+		
+		var assets = [PHAsset]()
+		fetchResult.enumerateObjects({
+			(asset, _, _) in
+			assets.append(asset)
+		})
+		
+		return assets
 	}
 	
-	func getImageForAsset(asset: PHAsset, size: CGSize, completion: @escaping ((UIImage?) -> Void)) {
-		let manager = PHCachingImageManager.default()
+	func startCachingAssets(assets: [PHAsset], sizes: [CGSize]) {
 		let options = PHImageRequestOptions()
 		options.resizeMode = .exact
 		options.deliveryMode = .opportunistic
-		manager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: options, resultHandler: {
+		for size in sizes {
+			self.imageManager.startCachingImages(for: assets, targetSize: size, contentMode: .aspectFill, options: options)
+		}
+	}
+	
+	func getImageForAsset(asset: PHAsset, size: CGSize, completion: @escaping ((UIImage?) -> Void)) {
+		let options = PHImageRequestOptions()
+		options.resizeMode = .exact
+		options.deliveryMode = .opportunistic
+		self.imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: options, resultHandler: {
 			(result, _) in
 			completion(result);
 		})
