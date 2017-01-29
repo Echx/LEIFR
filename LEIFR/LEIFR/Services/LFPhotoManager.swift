07a7466f60761385f4279450e509551000ff0991
Jinghan Wang <jinghan.w@icloud.com>
2017-01-29 16:57:15 +0800

    - fetch assets with given start and end time - thumbnail for the assets

---
+++ LEIFR/LEIFR/Services/LFPhotoManager.swift
@@ -0,0 +1,42 @@
+//
+//  LFPhotoManager.swift
+//  LEIFR
+//
+//  Created by Jinghan Wang on 29/1/17.
+//  Copyright © 2017 Echx. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class LFPhotoManager: NSObject {
+	static let shared = LFPhotoManager()
+	let thumbnailSize = CGSize(width: 200, height: 200)
+	
+	func fetchAssets(from fromDate: Date, till endDate: Date) -> PHFetchResult<PHAsset> {
+		let fetchOptions = PHFetchOptions()
+		fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+		fetchOptions.predicate = NSPredicate(format: "(creationDate >= %@) AND (creationDate <= %@)", fromDate as NSDate, endDate as NSDate)
+		
+		let fetchResult = PHAsset.fetchAssets(with: fetchOptions)
+		return fetchResult
+	}
+	
+	func getImageForAsset(asset: PHAsset, size: CGSize, completion: @escaping ((UIImage?) -> Void)) {
+		let manager = PHImageManager.default()
+		let options = PHImageRequestOptions()
+		options.resizeMode = .exact
+		options.deliveryMode = .opportunistic
+		manager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: options, resultHandler: {
+			(result, _) in
+			completion(result);
+		})
+	}
+}
+
+extension PHAsset {
+	func thumbnail(completion: @escaping ((UIImage?) -> Void)) {
+		let manager = LFPhotoManager.shared
+		manager.getImageForAsset(asset: self, size: manager.thumbnailSize, completion: completion)
+	}
+}
---
+++ LEIFR/LEIFRTests/LFPhotoManagerTest.swift
@@ -0,0 +1,56 @@
+//
+//  LFPhotoManagerTest.swift
+//  LEIFR
+//
+//  Created by Jinghan Wang on 29/1/17.
+//  Copyright © 2017 Echx. All rights reserved.
+//
+
+import XCTest
+@testable import LEIFR
+
+class LFPhotoManagerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testImageFetchCount() {
+		let manager = LFPhotoManager.shared
+		let fromDate = Date(timeIntervalSince1970: 0)
+		let endDate = Date()
+		let result = manager.fetchAssets(from: fromDate, till: endDate)
+		XCTAssertTrue(result.count != 0)
+    }
+	
+	func testImageFetchThumbnail() {
+		let manager = LFPhotoManager.shared
+		let fromDate = Date(timeIntervalSince1970: 0)
+		let endDate = Date()
+		let result = manager.fetchAssets(from: fromDate, till: endDate)
+		for i in 0..<result.count {
+			let asset = result.object(at: i)
+			asset.thumbnail(completion: {
+				result in
+				if let image = result {
+					XCTAssertEqual(image.size.width, 100, "width not correct")
+					XCTAssertEqual(image.size.height, 100, "height not correct")
+				}
+			})
+		}
+	}
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
