5a74c5054a6f6b98e95b115e92f525983a039682
Jinghan Wang <jinghan.w@icloud.com>
2017-01-30 22:06:08 +0800

    overlay to the map in photo detail

--- LEIFR/LEIFR/Views/Cells/LFMapCell.swift
+++ LEIFR/LEIFR/Views/Cells/LFMapCell.swift
@@ -11,10 +11,13 @@ import UIKit
 class LFMapCell: LFTableViewCell {
 
 	@IBOutlet var map: MKMapView!
+	fileprivate var overlayRenderer: LFGeoPointsOverlayRenderer!
 	
     override func awakeFromNib() {
         super.awakeFromNib()
         self.selectionStyle = .none
+		
+		self.configureMap()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -22,5 +25,26 @@ class LFMapCell: LFTableViewCell {
 
         // Configure the view for the selected state
     }
-    
+	
+	fileprivate func configureMap() {
+		map.delegate = self
+		
+		let overlay = LFGeoPointsOverlay()
+		map.add(overlay, level: .aboveRoads)
+	}
+	
+}
+
+extension LFMapCell: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		if overlay is LFGeoPointsOverlay {
+			if self.overlayRenderer == nil {
+				self.overlayRenderer = LFGeoPointsOverlayRenderer(overlay: overlay)
+			}
+			
+			return self.overlayRenderer
+		} else {
+			return MKOverlayRenderer(overlay: overlay)
+		}
+	}
 }
