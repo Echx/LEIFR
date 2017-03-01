35d46117a5449c28c1be23913397ef3449e38bd4
Jinghan Wang <jinghan.w@icloud.com>
2017-03-02 01:53:48 +0800

    import UIImage category for SVGs

---
+++ LEIFR/Pods/Target Support Files/UIImage+SVG/UIImage+SVG-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "PocketSVG.h"
+#import "SVGImageCache.h"
+#import "UIImage+SVG.h"
+
+FOUNDATION_EXPORT double UIImage_SVGVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIImage_SVGVersionString[];
+
