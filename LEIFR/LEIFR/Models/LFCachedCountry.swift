4029e9524f2c66f5d15eb0d0770ab9f2f352a187
Jinghan Wang <jinghan.w@icloud.com>
2017-03-02 02:14:45 +0800

    2-digit country code and localized name

--- LEIFR/LEIFR/Models/LFCachedCountry.swift
+++ LEIFR/LEIFR/Models/LFCachedCountry.swift
@@ -15,9 +15,23 @@ class LFCachedCountry: Object {
 	dynamic var continentCode: String = ""
 	dynamic var visited: Bool = false
 	
-// Specify properties to ignore (Realm won't persist these)
+	fileprivate lazy var locale: NSLocale = self.getCurrentLocale()
+	
+	func getCurrentLocale() -> NSLocale {
+		let identifier = Locale.identifier(fromComponents: [NSLocale.Key.countryCode.rawValue: self.code])
+		return NSLocale(localeIdentifier: identifier)
+	}
+	
+	func twoDigitCountryCode() -> String {
+		return (locale as Locale).regionCode?.lowercased() ?? ""
+	}
+	
+	func localizedName() -> String {
+		return locale.displayName(forKey: NSLocale.Key.countryCode, value: self.code) ?? "什么鬼"
+	}
     
-//  override static func ignoredProperties() -> [String] {
-//    return []
-//  }
+	override static func ignoredProperties() -> [String] {
+		return ["locale"]
+	}
+	
 }
