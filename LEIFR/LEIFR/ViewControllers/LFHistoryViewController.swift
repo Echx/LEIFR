1be98391ca4dba09c3726223ac85102c8fa0d3eb
Jinghan Wang <jinghan.w@icloud.com>
2017-03-13 05:17:31 +0800

    Fix a bug that record button state messed up when switch to other view

--- LEIFR/LEIFR/ViewControllers/LFHistoryViewController.swift
+++ LEIFR/LEIFR/ViewControllers/LFHistoryViewController.swift
@@ -23,7 +23,12 @@ class LFHistoryViewController: LFViewController {
         
         self.configureMap()
     }
-    
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated);
+		recordButton.isSelected = LFGeoRecordManager.shared.isRecording;
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
