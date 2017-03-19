fedef4f6d1d74f2abc98bad2e45fbc01ab737385
Lei Mingyu <uygnim.iel@gmail.com>
2017-03-20 04:19:36 +0800

    add path playing manager

---
+++ LEIFR/LEIFR/Services/LFPathPlayingManager.swift
@@ -0,0 +1,13 @@
+//
+//  LFPathPlayingManager.swift
+//  LEIFR
+//
+//  Created by Lei Mingyu on 20/3/17.
+//  Copyright © 2017 Echx. All rights reserved.
+//
+
+import UIKit
+
+class LFPathPlayingManager: NSObject {
+    
+}
